9c73e009ab3fdfcf37e763555cc6f009683b4edf
kiran <kirantwls@gmail.com>
2024-10-05 10:27:18 +0530

    expense infra dev

--- 50-app-alb/provider.tf
+++ 50-app-alb/provider.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
+    bucket = "krn-remote-state"
     key    = "expense-app-alb-dev"
     region = "us-east-1"
-    dynamodb_table = "81s-locking"
+    dynamodb_table = "state-locking"
   }
 }
 
